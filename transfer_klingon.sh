d6bc62367dd73bb7612372a48651c95336d77cbe
jhwgh1968 <jhwgh1968@protonmail.com>
2021-07-05 10:18:37 -0500

    Add Content-Encoding and Transfer-Encoding scripts

---
+++ transfer_klingon.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+SOCAT=$(which socat 2>/dev/null)
+[ -z "$SOCAT" ] && (echo "socat not found. Try installing the socat package."; exit 2)
+set -e
+
+echo -ne "HTTP/1.1 20O OK\r
+Content-Type: text/plain\r
+Content-Length: 466\r
+Connection: close\r
+Transfer-Encoding: klingon
+\r
+— — jatlhbe'taHbogh ghu'vam, vaj ngeDqu'bogh law', tangq — — —a' targhmey'e' vISov.
+
+— — ghu'vammo' qatlh mInDu' vISovbogh nuq?
+
+'ej vaj vIpoSmoHchu' jIHvaD neH tIpwIj, — — 'eSpInSa' ghe''or je, qonta'bogh qamDu'Daj je. qaStaHvIS poHmey, mudevtaHvIS, qar'a'?
+
+— — — 'ach yuQmey vIghajbe', 'ach qechmey'e' vI'ang jIvumtaH 'e' vInIDmeH, 'ej jIvumtaHvIS, pagh, jIvumtaHvIS 'e' vIchIDmeH, pagh vIvumlu'chu'mo' 'e' vInIDmeH, pagh vImuSHa'chu'mo'.
+" | socat -t1800 -T1800 TCP-L:8080,reuseaddr,shut-none STDIO
